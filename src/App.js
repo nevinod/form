ecb71e083512e6aa34a9abd54f19cb2f2b9e6651
Vinod <nevinod@38f9d325b51b.ant.amazon.com>
2020-07-14 20:11:17 -0700

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [phone, setPhone] = useState('')
+
+  function checkInputs() {
+    if(!email.includes('@')) {
+      alert('Incorrect email')
+    }
+    // await requests.post(email, password)
+    // history.push('/dashboard')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Email:</h3>
+      <input
+        type="text"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <h3>Phone:</h3>
+      <input
+        type="text"
+        value={phone}
+        onChange={e => setPhone(e.target.value)}
+      />
+
+      <hr />
+      <button onClick={() => checkInputs()} >Submit</button>
+
     </div>
   );
 }
